17e7f3c318216767d6e846216ef20d5a60efc939
yvromero <yvonne.romero@uds.edu.py>
2023-09-11 21:29:20 -0400

    Configure index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,14 @@
+import { ShopLayout } from '@/components/layouts';
 import type { NextPage } from 'next';
-
+import { Typography } from '@mui/material';
 const Home: NextPage = () => {
   return (
 
-            <h2>
-              Margaret Art Store 
-            </h2>
-
+      <ShopLayout title={'Margaret Art Store - Home'} 
+      pageDescription={'El arte tiene que estar accesible para todos.'} >
+        <Typography variant='h1' component='h1'>E-Store</Typography>
+        <Typography variant='h2' sx={{ mb:1 }}>Todos los productos</Typography>
+      </ShopLayout>
   )
 }
 
