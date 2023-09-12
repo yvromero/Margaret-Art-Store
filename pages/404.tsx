2db2ca3120f58b0349321647500b07eaa9eed83f
yvromero <yvonne.romero@uds.edu.py>
2023-09-12 16:09:52 -0400

    Create custom 404 error page

---
+++ pages/404.tsx
@@ -0,0 +1,24 @@
+import { ShopLayout } from '../components/layouts';
+import { Box, Typography } from '@mui/material';
+
+const Custom404 = () => {
+  return (
+    <ShopLayout title= 'Page not found' pageDescription='No hay nada que mostrar aqui'>
+
+    <Box 
+        display='flex' 
+        justifyContent='center' 
+        alignItems='center' 
+        height='calc(100vh - 200px)'
+        sx={{ flexDirection : { xs: 'column', sm: 'column', md: 'row'}}}
+    >
+      <Typography variant='h2' component='h2' fontSize={60} fontWeight={160}>404 |</Typography>
+      <Typography marginLeft={2}>Sorry, we couldn't find that page</Typography>
+    </Box>
+
+  </ShopLayout>
+  
+  )
+}
+
+export default Custom404;
