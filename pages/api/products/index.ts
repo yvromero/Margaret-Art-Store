f8a43b4e7d69c41657aff542f075e8e92b2f7253
yvromero <yvonne.romero@uds.edu.py>
2023-09-21 13:41:00 -0400

    Create query by products

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -26,7 +26,9 @@ function handler (req: NextApiRequest, res: NextApiResponse<Data>) {
 const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
     await db.connect();
-    const products = await Product.find().lean();
+    const products = await Product.find()
+                                .select('title images price inStock slug -_id')
+                                .lean();
     await db.disconnect();
 
     return res.status(200).json( products );
