cc066007b0ccd31df2e687150f24afeedc9396ab
yvromero <yvonne.romero@uds.edu.py>
2023-10-03 23:19:16 -0300

    Remove address from cookies

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -93,6 +93,18 @@ export const AuthProvider:FC<AuthProviderProps> = ({ children }) => {
     const logoutUser = () => {
         Cookies.remove('token');
         Cookies.remove('cart');
+        Cookies.remove('firstName');
+        Cookies.remove('lastName');
+        Cookies.remove('documentType');
+        Cookies.remove('documentNumber');
+        Cookies.remove('country');
+        Cookies.remove('region');
+        Cookies.remove('city');
+        Cookies.remove('address');
+        Cookies.remove('address2' || '');
+        Cookies.remove('zip');
+        Cookies.remove('phone');
+        Cookies.remove('email');
         router.reload();
     }
 
