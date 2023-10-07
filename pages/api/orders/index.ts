d10900ef125cc9b7081e89942d6c8f6f808661f1
yvromero <yvonne.romero@uds.edu.py>
2023-10-07 12:00:16 -0300

    Add changes to the index/order

--- pages/api/orders/index.ts
+++ pages/api/orders/index.ts
@@ -63,10 +63,12 @@ const createOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
 
 
-    // Todo bien hasta este punto
+    // Impactar en la BD
 
     const userId = session.user._id;
     const newOrder = new Order({ ...req.body, isPaid: false, user: userId });
+    newOrder.total = Math.round( newOrder.total * 100 ) / 100;
+
     await newOrder.save();
     await db.disconnect();
     
