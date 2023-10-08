ed1655dca52082329b32110a51d9fdfb0c25394f
yvromero <yvonne.romero@uds.edu.py>
2023-10-07 23:07:08 -0300

    Validate orderId and transactionId to update the operation

--- pages/api/orders/pay.ts
+++ pages/api/orders/pay.ts
@@ -1,8 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import axios from 'axios';
-// import { IPaypal } from '../../../interfaces';
-// import { db } from '../../../database';
-// import { Order } from '../../../models';
+import { IPaypal } from '@/interfaces';
+import { db } from '../../../database';
+import { Order } from '../../../models';
 
 
 type Data = {
@@ -52,6 +52,8 @@ const getPaypalBearerToken = async():Promise<string|null>=> {
 
 const payOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
+   //  TODO: Validar que la sesion corresponda al usuario que genero la orden
+
     const paypalBearerToken = await getPaypalBearerToken();
 
     if ( !paypalBearerToken ) {
@@ -61,15 +63,39 @@ const payOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     // Realizar la peticion con el token para confirmar con Paypal el estado de la operacion
     const { transactionId = '', orderId = '' } = req.body;
 
-    const { data } = await axios.get (`${ process.env.PAYPAL_ORDERS_URL }/${ transactionId }`, {
+    // Validar que el estado de la transaccion en Paypal sea Completado
+    const { data } = await axios.get<IPaypal.PaypalOrderStatusResponse> (`${ process.env.PAYPAL_ORDERS_URL }/${ transactionId }`, {
         headers: {
             'Authorization': `Bearer ${ paypalBearerToken }`, 
         }
     });
+    
+    if ( data.status !== 'COMPLETED' ) {
+        return res.status(401).json({ message: 'Orden no reconocida' })
+    }
+
+    // Validar que la orden en Mongo se haya generado
+    await db.connect();
+    const dbOrder = await Order.findById( orderId );
+
+    if ( !dbOrder ) {
+    await db.disconnect();
+        return res.status(401).json({ message: 'Orden no encontrada en la base de datos' });
+    }
+
+    // Validar que monto el monto de la orden en Mongo sea el mismo que el monto de la transaccion
+    if ( dbOrder.total !== Number(data.purchase_units[0].amount.value) ) {
+        await db.disconnect();
+        return res.status(400).json({ message: 'El monto del pago no corresponde con el monto de la orden generada'});
+
+    }
+
+    dbOrder.transactionId = transactionId;
+    dbOrder.isPaid = true;
+    await dbOrder.save(); //Actualizar a true
+
+    await db.disconnect();
 
 
-
-
-
-    return res.status(200).json({ message: paypalBearerToken })
+    return res.status(200).json({ message: 'Transacción completada' })
 }
