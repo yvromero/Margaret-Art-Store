1a8b88ed09a55bc4f703e309c302d56bbd1ef1bb
yvromero <yvonne.romero@uds.edu.py>
2023-10-24 02:02:27 -0300

    Add Hi username in homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import type { NextPage } from 'next';
 import { useProducts } from '../hooks';
-import { Typography } from '@mui/material';
+import { Typography,Box } from '@mui/material';
 import { ShopLayout } from '@/components/layouts';
 import { ProductList } from '@/components/products';
 import { FullScreenLoading } from '@/components/ui';
 
+import { useSession } from 'next-auth/react';
+
 
 const HomePage: NextPage = () => {
 
-
+  const { data: session } = useSession();
   const { products, isLoading } = useProducts ('/products');
 
   return (
@@ -17,6 +19,20 @@ const HomePage: NextPage = () => {
           title={'Margaret Art Store'} 
           pageDescription={'El arte tiene que estar accesible para todos.'}
       >
+
+      <Box display="flex" justifyContent={'end'}>
+          {session && session.user ? (
+          <Typography variant="body1">
+              ¡Hola,{' '}
+              <span style={{ color: 'darkslategray', fontWeight: 'bold' }}>
+                  {session.user.name}
+              </span>
+              !
+          </Typography>
+              ) : null}
+      </Box>
+
+
         <Typography variant='h1' component='h1'>GALERIA</Typography>
         <Typography variant='h2' sx={{ mb:2 }}>Todos los cuadros</Typography>
 
